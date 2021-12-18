7673a5f1350c551f15ebe962e950ce6d3f3ee9e9
Denys <den32@seznam.cz>
2021-12-18 22:33:35 +0100

    some_changes

--- new_0.js
+++ new_0.js
@@ -1,1 +1,20 @@
 document.body.innerHTML = <h1>JSjsj</h1>
+
+function checkObj(obj, checkProp) {
+    if (obj.hasOwnProperty(checkProp)) {
+      return obj[checkProp];
+    } else {
+      return "Not Found";
+    }
+  }
+
+//   Example
+
+// const myObj = {
+//   top: "hat",
+//   bottom: "pants"
+// };
+
+// myObj.hasOwnProperty("top");
+// myObj.hasOwnProperty("middle");
+// The first hasOwnProperty returns true, while the second returns false.
