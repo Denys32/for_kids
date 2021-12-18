e07064a4dbb066b2fa54695e005853c0b29e7c60
Denys <den32@seznam.cz>
2021-12-18 22:44:16 +0100

    change_new_0

--- new_0.js
+++ new_0.js
@@ -18,3 +18,36 @@ function checkObj(obj, checkProp) {
 // myObj.hasOwnProperty("top");
 // myObj.hasOwnProperty("middle");
 // The first hasOwnProperty returns true, while the second returns false.
+
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },{
+      "artist": "Rammshtein",
+      "title": "best",
+      "release_year": 1992,
+      "formats": ["cd", "dvd", "mp3"]
+    }
+  ];
+
+//   const ourMusic = [
+//     {
+//       "artist": "Daft Punk",
+//       "title": "Homework",
+//       "release_year": 1997,
+//       "formats": [ 
+//         "CD", 
+//         "Cassette", 
+//         "LP"
+//       ],
+//       "gold": true
+//     }
+//   ];
