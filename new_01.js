84b6c6f3a8fab7a60abf26216304ba568ad47671
Denys <den32@seznam.cz>
2022-01-11 14:00:28 +0100

    new

--- new_01.js
+++ new_01.js
@@ -277,16 +277,17 @@
 //       }
 //   }
 
-const myStorage = {
-    "car": {
-      "inside": {
-        "glove box": "maps",
-        "passenger seat": "crumbs"
-       },
-      "outside": {
-        "trunk": "jack"
-      }
-    }
-  };
+// const myStorage = {
+//     "car": {
+//       "inside": {
+//         "glove box": "maps",
+//         "passenger seat": "crumbs"
+//        },
+//       "outside": {
+//         "trunk": "jack"
+//       }
+//     }
+//   };
   
-  const gloveBoxContents = myStorage.car.inside["glove box"];
+//   const gloveBoxContents = myStorage.car.inside["glove box"];
+
